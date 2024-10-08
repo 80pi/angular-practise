2c9cff84ae22a08ddf248dd72dfc5f400dcaaca1
80pi <gopivenkataajay5771@gmail.com>
2024-10-08 19:17:27 +0530

    feat: adding new static component and adding to flow

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { StaticPageComponent } from './static-page/static-page.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, StaticPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'practise angular';
+  message = 'parent message';
 }
---
+++ src/app/static-page/static-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-static-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './static-page.component.html',
+  styleUrl: './static-page.component.css',
+})
+export class StaticPageComponent {}
